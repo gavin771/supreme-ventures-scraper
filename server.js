e4d96c2c6e8e6f41ee45b23c958265990f200313
Gavin Samuels <gavin.samuels10@gmail.com>
2019-10-06 20:53:14 -0500

    created seperate files for cloud deployment and local development.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const getResults = require("./getResults");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+const app = express();
+app.use(helmet());
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Up and running!");
+});
+
+app.get("/results", async (req, res) => {
+  try {
+    const gameResults = await getResults();
+
+    res.status(200).json(gameResults);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.listen(process.env.PORT || 3030, () =>
+  console.log(`Up and running on port ${process.env.PORT || 3030}!`)
+);
