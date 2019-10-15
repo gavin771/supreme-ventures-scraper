f5c3bf2fa9320f7b4317a741e48eafa20baeaf85
Gavin Samuels <gavin.samuels10@gmail.com>
2019-10-14 23:56:08 -0500

    moved selectors to a new file

---
+++ gameConfig.js
@@ -0,0 +1,65 @@
+module.exports = [
+  {
+    title: "Cash Pot",
+    key: "cash-pot",
+    numbersSelector: ".game.cashpot .result-number",
+    dateSelector: ".game.cashpot .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.cashpot .game-result h5"
+  },
+  {
+    title: "Pick 2",
+    key: "pick-2",
+    numbersSelector: ".game.pick2 .result-number",
+    dateSelector: ".game.pick2 .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.pick2 .game-result h5"
+  },
+  {
+    title: "Pick 3",
+    key: "pick-3",
+    numbersSelector: ".game.pick3 .result-number",
+    dateSelector: ".game.pick3 .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.pick3 .game-result h5"
+  },
+  {
+    title: "Pick 4",
+    key: "pick-4",
+    numbersSelector: ".game.pick4 .result-number",
+    dateSelector: ".game.pick4 .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.pick4 .game-result h5"
+  },
+  {
+    title: "Lucky 5",
+    key: "lucky-5",
+    numbersSelector: ".game.lucky5 .result-number",
+    dateSelector: ".game.lucky5 .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.lucky5 .game-result h5"
+  },
+  {
+    title: "Dollaz",
+    key: "dollaz",
+    numbersSelector: ".game.dollaz .result-number",
+    dateSelector: ".game.dollaz .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.dollaz .game-result h5"
+  },
+  {
+    title: "Top Draw",
+    key: "top-draw",
+    numbersSelector: ".game.topdraw .result-number",
+    dateSelector: ".game.cashpot .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.cashpot .game-result h5"
+  },
+  {
+    title: "Lotto",
+    key: "lotto",
+    numbersSelector: ".game.lotto .result-number",
+    dateSelector: ".game.lotto .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.lotto .game-result h5"
+  },
+  {
+    title: "Super Lotto",
+    key: "super-lotto",
+    numbersSelector: ".game.superlotto .result-number",
+    dateSelector: ".game.superlotto .game-result h4:nth-child(1)",
+    drawNameSelector: ".game.superlotto .game-result h5"
+  }
+];
