5e2f09c547f51e399d63e89d96773de83fbda854
Gavin Samuels <gavin.samuels10@gmail.com>
2019-10-23 01:13:00 -0500

    updated selectors

--- gameConfig.js
+++ gameConfig.js
@@ -2,64 +2,103 @@ module.exports = [
   {
     title: "Cash Pot",
     key: "cash-pot",
-    numbersSelector: ".game.cashpot .result-number",
-    dateSelector: ".game.cashpot .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.cashpot .game-result h5"
+    numbersSelector:
+      ".game-container:nth-child(1) .game.cashpot .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.cashpot .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.cashpot .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.cashpot .jackpot",
+    url: "https://supremeventures.com/game-results/?action=cashpot"
   },
   {
     title: "Pick 2",
     key: "pick-2",
-    numbersSelector: ".game.pick2 .result-number",
-    dateSelector: ".game.pick2 .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.pick2 .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.pick2 .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.pick2 .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.pick2 .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.pick2 .jackpot",
+    url: "https://supremeventures.com/game-results/?action=pick2"
   },
   {
     title: "Pick 3",
     key: "pick-3",
-    numbersSelector: ".game.pick3 .result-number",
-    dateSelector: ".game.pick3 .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.pick3 .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.pick3 .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.pick3 .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.pick3 .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.pick3 .jackpot",
+    url: "https://supremeventures.com/game-results/?action=pick3"
   },
   {
     title: "Pick 4",
     key: "pick-4",
-    numbersSelector: ".game.pick4 .result-number",
-    dateSelector: ".game.pick4 .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.pick4 .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.pick4 .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.pick4 .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.pick4 .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.pick4 .jackpot",
+    url: "https://supremeventures.com/game-results/?action=pick4"
   },
   {
     title: "Lucky 5",
     key: "lucky-5",
-    numbersSelector: ".game.lucky5 .result-number",
-    dateSelector: ".game.lucky5 .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.lucky5 .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.lucky5 .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.lucky5 .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.lucky5 .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.lucky5 .jackpot",
+    url: "https://supremeventures.com/game-results/?action=lucky5"
   },
   {
     title: "Dollaz",
     key: "dollaz",
-    numbersSelector: ".game.dollaz .result-number",
-    dateSelector: ".game.dollaz .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.dollaz .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.dollaz .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.dollaz .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.dollaz .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.dollaz .jackpot",
+    url: "https://supremeventures.com/game-results/?action=dollaz"
   },
   {
     title: "Top Draw",
     key: "top-draw",
-    numbersSelector: ".game.topdraw .result-number",
-    dateSelector: ".game.cashpot .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.cashpot .game-result h5"
+    numbersSelector:
+      ".game-container:nth-child(1) .game.topdraw .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.topdraw .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.topdraw .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.topdraw .jackpot",
+    url: "https://supremeventures.com/game-results/?action=topdraw"
   },
   {
     title: "Lotto",
     key: "lotto",
-    numbersSelector: ".game.lotto .result-number",
-    dateSelector: ".game.lotto .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.lotto .game-result h5"
+    numbersSelector: ".game-container:nth-child(1) .game.lotto .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.lotto .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.lotto .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.lotto .jackpot",
+    url: "https://supremeventures.com/game-results/?action=lotto"
   },
   {
     title: "Super Lotto",
     key: "super-lotto",
-    numbersSelector: ".game.superlotto .result-number",
-    dateSelector: ".game.superlotto .game-result h4:nth-child(1)",
-    drawNameSelector: ".game.superlotto .game-result h5"
+    numbersSelector:
+      ".game-container:nth-child(1) .game.superlotto .result-number",
+    dateSelector:
+      ".game-container:nth-child(1) .game.superlotto .game-result h5:nth-child(1)",
+    drawNameSelector:
+      ".game-container:nth-child(1)  .game.superlotto .game-result > h4:nth-of-type(1)",
+    jackpotSelector: ".game-container:nth-child(1) .game.superlotto .jackpot",
+    url: "https://supremeventures.com/game-results/?action=superlotto"
   }
 ];
