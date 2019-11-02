851b8062d4b2622190487c209233efa48e71730f
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-02 00:12:36 -0500

    added symbols for cashpot

--- cashpot-mappings.js
+++ cashpot-mappings.js
@@ -1,6 +1,146 @@
 module.exports = [
   {
+    symbol: "duppy",
+    number: 1
+  },
+  {
+    symbol: "small fire",
+    number: 2
+  },
+  {
+    symbol: "dead",
+    number: 3
+  },
+  {
+    symbol: "egg",
+    number: 4
+  },
+  {
+    symbol: "thief",
+    number: 5
+  },
+  {
+    symbol: "strong man",
+    number: 6
+  },
+  {
+    symbol: "married woman",
+    number: 7
+  },
+  {
     symbol: "belly",
     number: 8
+  },
+  {
+    symbol: "cow",
+    number: 9
+  },
+  {
+    symbol: "small house",
+    number: 10
+  },
+  {
+    symbol: "dog",
+    number: 11
+  },
+  {
+    symbol: "head",
+    number: 12
+  },
+  {
+    symbol: "police",
+    number: 13
+  },
+  {
+    symbol: "mouth",
+    number: 14
+  },
+  {
+    symbol: "rat",
+    number: 15
+  },
+  {
+    symbol: "young girl",
+    number: 16
+  },
+  {
+    symbol: "chinese man",
+    number: 17
+  },
+  {
+    symbol: "race horse",
+    number: 18
+  },
+  {
+    symbol: "silver money",
+    number: 19
+  },
+  {
+    symbol: "sick person",
+    number: 20
+  },
+  {
+    symbol: "bad girl",
+    number: 21
+  },
+  {
+    symbol: "white woman",
+    number: 22
+  },
+  {
+    symbol: "black man",
+    number: 23
+  },
+  {
+    symbol: "fresh water",
+    number: 24
+  },
+  {
+    symbol: "john crow",
+    number: 25
+  },
+  {
+    symbol: "white man",
+    number: 26
+  },
+  {
+    symbol: "big fire",
+    number: 27
+  },
+  {
+    symbol: "fowl",
+    number: 28
+  },
+  {
+    symbol: "parson",
+    number: 29
+  },
+  {
+    symbol: "fish",
+    number: 30
+  },
+  {
+    symbol: "lizard",
+    number: 31
+  },
+  {
+    symbol: "gold",
+    number: 32
+  },
+  {
+    symbol: "big house",
+    number: 33
+  },
+  {
+    symbol: "baby",
+    number: 34
+  },
+  {
+    symbol: "goat",
+    number: 35
+  },
+  {
+    symbol: "old lady",
+    number: 36
   }
 ];
