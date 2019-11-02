c54aebfc6e0669e7fc32951332319c650da66cf7
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-01 20:30:02 -0500

    added schedule data for cashpot draws

---
+++ jobConfig.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    name: "Cash-Pot_8-30AM",
+    description: "Cashpot 8:30AM draw",
+    schedule: "40 8 * * *",
+    data: { action: "store", game: "cash-pot" }
+  },
+  {
+    name: "Cash-Pot_10-30AM",
+    description: "Cashpot 10:30AM draw",
+    schedule: "40 10 * * *",
+    data: { action: "store", game: "cash-pot" }
+  },
+  {
+    name: "Cash-Pot_1-00PM",
+    description: "Cashpot 1:00PM draw",
+    schedule: "10 13 * * *",
+    data: { action: "store", game: "cash-pot" }
+  },
+  {
+    name: "Cash-Pot_3-00PM",
+    description: "Cashpot 3:00PM draw",
+    schedule: "10 15 * * *",
+    data: { action: "store", game: "cash-pot" }
+  },
+  {
+    name: "Cash-Pot_5-00PM",
+    description: "Cashpot 5:00PM draw",
+    schedule: "10 17 * * *",
+    data: { action: "store", game: "cash-pot" }
+  },
+  {
+    name: "Cash-Pot_8-25PM",
+    description: "Cashpot 8:25PM draw",
+    schedule: "35 20 * * *",
+    data: { action: "store", game: "cash-pot" }
+  }
+];
