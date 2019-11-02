3c9a32362b803fff54c2f9dea82cc6d4aab52442
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-01 22:49:51 -0500

    added delete job feature

--- jobs.js
+++ jobs.js
@@ -9,47 +9,77 @@ const createJobs = async () => {
     process.env.LOCATION_ID
   );
 
-  return await Promise.all(
-    jobConfig.forEach(payload => {
-      return new Promise(async (resolve, reject) => {
-        try {
-          const job = {
-            httpTarget: {
-              httpMethod: "POST",
-              uri: process.env.API,
-              body: Buffer.from(JSON.stringify(payload.data))
-            },
-            schedule: payload.schedule,
-            timeZone: "Jamaica",
-            retryConfig: {
-              retryCount: 5
-            },
-            description: payload.description
-          };
+  await deleteJobs();
 
-          if (payload.name) {
-            job.name = `projects/${process.env.PROJECT_ID}/locations/${process.env.LOCATION_ID}/jobs/${payload.name}`;
-          }
+  jobConfig.forEach(async payload => {
+    try {
+      const job = {
+        httpTarget: {
+          httpMethod: "POST",
+          uri: process.env.API,
+          body: Buffer.from(JSON.stringify(payload.data))
+        },
+        schedule: payload.schedule,
+        timeZone: "Jamaica",
+        retryConfig: {
+          retryCount: 5
+        },
+        description: payload.description
+      };
 
-          const request = {
-            job: job,
-            parent: parent
-          };
+      if (payload.name) {
+        job.name = `projects/${process.env.PROJECT_ID}/locations/${process.env.LOCATION_ID}/jobs/${payload.name}`;
+      }
 
-          const response = await client.createJob(request);
+      const request = {
+        job: job,
+        parent: parent
+      };
 
-          console.log(
-            `Returning response from createCommercialInvoiceReminder: ${JSON.stringify(
-              response
-            )}`
-          );
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      });
-    })
+      const response = await client.createJob(request);
+
+      console.log(
+        `Returning response from createCommercialInvoiceReminder: ${JSON.stringify(
+          response
+        )}`
+      );
+    } catch (e) {
+      throw e;
+    }
+  });
+  return true;
+};
+
+const listJobs = () => {
+  const client = new scheduler.CloudSchedulerClient();
+  const parent = client.locationPath(
+    process.env.PROJECT_ID,
+    process.env.LOCATION_ID
   );
+  const request = {
+    parent: parent
+  };
+  return client.listJobs(request);
+};
+
+const deleteJobs = async () => {
+  const client = new scheduler.CloudSchedulerClient();
+  const parent = client.locationPath(
+    process.env.PROJECT_ID,
+    process.env.LOCATION_ID
+  );
+  const [jobs] = await listJobs();
+
+  jobs.forEach(async job => {
+    try {
+      const request = {
+        name: job.name
+      };
+      await client.deleteJob(request);
+    } catch (e) {
+      throw e;
+    }
+  });
 };
 
 module.exports.createJobs = createJobs;
